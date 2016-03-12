3b427775d1ddfe4d812af70f805ee330d767f49d
kochetygovamaria <kochetygova.m@husky.neu.edu>
2016-03-12 00:10:15 -0500

    Added files via upload

---
+++ Count a.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HowOften
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Please any phrase");
+			string input = Console.ReadLine ();
+			int count=0;
+
+			foreach (char item in input) {
+
+
+				if (item == 'a') {
+					count++;
+				}
+			
+			
+			}
+
+			Console.WriteLine (count);
+			Console.ReadLine ();
+		}
+	}
+
+	}
+
