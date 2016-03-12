8eb2b6067e29cfd24349daa38888be89a3267a85
kochetygovamaria <kochetygova.m@husky.neu.edu>
2016-03-11 23:17:02 -0500

    Added files via upload

---
+++ randomNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace RandomNumbers
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hi! Random number will be generated");
+			for (int i = 0; i < 10; i++) {
+				RandomNumber ();
+
+
+			}
+		
+
+		}
+		public static Random number=new Random();
+		public static void RandomNumber(){
+			int n = number.Next ();
+			Console.WriteLine (n);
+			Console.ReadLine ();
+		
+		}
+
+	}
+}
