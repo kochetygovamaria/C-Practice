e3783cd0fe675c98049d81780ec39e68a3306532
kochetygovamaria <kochetygova.m@husky.neu.edu>
2016-03-11 23:20:56 -0500

    Added files via upload

---
+++ Practices print number before 42.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Numbers
+{
+	
+		class MainClass
+		{
+			public static void Main (string[] args)
+			{
+
+				int m;
+				Console.WriteLine ("Please enter the size of list");
+				try{
+					m = Convert.ToInt32 (Console.ReadLine ());
+					int[] a = new int[m];
+
+
+
+					Console.WriteLine ("Please enter the number into your list");
+
+					for (int i = 0; i < m; i++) {
+
+						a [i] = Convert.ToInt32 (Console.ReadLine ());
+
+					}
+
+					foreach (int o in a) {
+						if (o == 42) {
+							break;
+
+
+						}
+
+						Console.Write ("\n");
+
+						Console.WriteLine (o);}
+
+				}
+
+
+				catch(FormatException ex)
+
+				{
+
+					Console.WriteLine ("Please enter the number");
+				}
+
+
+
+				Console.ReadLine ();
+			}
+		}
+
+	}
+	
+
